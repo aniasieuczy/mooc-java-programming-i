a63f9c120e2222ef742a1a12bbac9550ae8dfc94
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-26 19:40:58 +0100

    Add files via upload

---
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class NumberAndSumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int count = 0;
+        int sum = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int userInput = Integer.valueOf(scanner.nextLine());
+            
+            if(userInput != 0) {
+                count = count + 1;
+                sum = sum + userInput;
+            } else {
+                System.out.println("Number of numbers: " + count);
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+            }
+        }
+
+    }
+}
