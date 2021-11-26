82791fd7439b179005093561aac16daa28cfa719
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-26 19:22:26 +0100

    add SimpleCalculator

---
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        int sum = first + second;
+        int subtraction = first - second;
+        int multiplication = first * second;
+        double quotient = (double)first / second;
+        System.out.println(first + " + " + second + " = " + sum);
+        System.out.println(first + " - " + second + " = " + subtraction);
+        System.out.println(first + " * " + second + " = " + multiplication);
+        System.out.println(first + " / " + second + " = " + quotient);
+        
+
+    }
+}
