42e9520c7517e9530a24e908dafa53f04702a8d6
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-27 10:41:25 +0100

    Add files via upload

---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+
+    public static void main(String[] args) {
+        
+        // This exercise is worth five exercise points, and it is 
+        // gradually extended part by part.
+        
+        // If you want, you can send this exercise to the server
+        // when it's just partially done. In that case the server will complain about 
+        // the parts you haven't done, but you'll get points for the finished parts.
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give numbers:");
+        int sum = 0;
+        int count = 0;
+        double average = 0;
+        int odd = 0;
+        int even = 0;
+ 
+        while (true) {
+            int userNumber = Integer.valueOf(scanner.nextLine());
+            
+            if(userNumber == -1) {
+                break;
+            } 
+            
+            if(userNumber != -1) {
+                sum = sum + userNumber;
+                count ++;
+                average = (double)sum/count;
+            }
+             
+            if(userNumber % 2 == 0) {
+                even++;
+            }
+            
+            if(userNumber % 2 !=0) {
+                odd++;
+            }
+        }
+        
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + average);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+    }
+}
