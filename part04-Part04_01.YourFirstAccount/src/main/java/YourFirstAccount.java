f3f955232699e3eed5815754154bd60c0493fc5c
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-27 11:05:15 +0100

    Add files via upload

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,14 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account aniasAccount = new Account ("Ania account", 100); 
+        
+        aniasAccount.deposit(20);
+        System.out.println("End state");
+        System.out.println(aniasAccount);
+        
+    }
+}
