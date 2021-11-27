fe5de49ab06907135267f7b7cea6c487504c7299
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-27 10:51:04 +0100

    Add files via upload

---
+++ part03-Part03_19.IndexWasNotFound/src/main/java/IndexWasNotFound.java
@@ -0,0 +1,47 @@
+
+
+import java.util.Scanner;
+
+public class IndexWasNotFound {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[10];
+        array[0] = 6;
+        array[1] = 2;
+        array[2] = 8;
+        array[3] = 1;
+        array[4] = 3;
+        array[5] = 0;
+        array[6] = 9;
+        array[7] = 7;
+
+        System.out.print("Search for? ");
+        int searching = Integer.valueOf(scanner.nextLine());
+
+        boolean found = true;
+        int index = 0;
+        int number;
+
+        while (index < array.length) {
+            number = array[index];
+            if (number == searching) {
+                System.out.println(searching + " is at index " + index +".");
+                found = true;
+                break;
+            }
+            index = index + 1;
+            found = false;
+        }
+            
+            
+        
+              if (found == false) {
+              System.out.println(searching + " was not found.");
+              }
+        }
+    }
+
+    
+
