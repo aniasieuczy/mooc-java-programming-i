4db66d9951d35fa4dc84bbc66f33534b5e34b334
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-27 10:40:34 +0100

    Add files via upload

---
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class SumOfASequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.println("Last number?");
+        int number = Integer.valueOf(scanner.nextLine());
+      
+        int sum = 0;
+        while(true) {
+        
+            for(int i = 1; i <= number; i++) {
+                sum = sum + i;
+            }
+            System.out.println("The sum is " + sum);
+            break;
+        }
+    }
+}
