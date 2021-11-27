7f325143e68d81ad9e2c361db44e1e0cd1da0328
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-27 10:49:02 +0100

    Add files via upload

---
+++ part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum = 0;
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+            sum = input + sum;
+        }
+        
+
+        System.out.println("Sum: " + sum);
+
+        // perform the calculation of the sum of the numbers in the list here
+    }
+}
