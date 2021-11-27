a63074bd1f6e6e8cc74072447e980e3a2b488a05
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-27 11:17:37 +0100

    Add files via upload

---
+++ part05-Part05_12.Song/src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // you can write code here for testing your program
+
+        Song jackSparrow = new Song("The Lonely Island", "Jack Sparrow", 196);
+        Song anotherSparrow = new Song("The Lonely Island", "Jack Sparrow", 196);
+
+        if (jackSparrow.equals(anotherSparrow)) {
+            System.out.println("Songs are equal." + jackSparrow.getName());
+        } else {
+            System.out.println("Strange things are afoot.");
+        }
+        
+        
+    }
+}
