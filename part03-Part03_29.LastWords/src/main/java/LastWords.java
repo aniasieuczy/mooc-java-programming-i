ed1d5796178fbcbcf1d1c5c4d873cb5eef887615
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-27 11:04:11 +0100

    Add files via upload

---
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class LastWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String input = scanner.nextLine();
+            
+            if(input.equals("")) {
+                break;
+            }
+            
+            String [] pieces = input.split(" ");
+            for(int i = 0; i < pieces.length; i++) {
+                int lastPiece = pieces.length - 1;
+                System.out.println(pieces[lastPiece]);
+                break;
+            }
+            
+        }
+
+
+    }
+}
